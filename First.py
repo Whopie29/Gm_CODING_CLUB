1c2efa5077efbaf6e10812befe82ea2ea807fdf8
GAURAV MALIK <130136530+Whopie29@users.noreply.github.com>
2023-09-18 13:28:37 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,40 @@
+
+class Father():
+	
+	
+	def __init__(self):
+		self.value = "Hello Python"
+		
+	
+	def show(self):
+		print(self.value)
+		
+
+class Son(Father):
+	
+	
+	def __init__(self):
+		self.value = "Hey Python"
+		
+	
+	def show(self):
+		print(self.value)
+		
+
+obj1 = Father()
+obj2 = Son()
+
+obj1.show()
+obj2.show()
+
+
+
+
+
+
+Q3) (b)
+Q4) (c)
+Q5) (INTEGER VALUE)
+
+
+
